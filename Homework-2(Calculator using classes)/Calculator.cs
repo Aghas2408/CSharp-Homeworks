ac00a68cea02233db8a2075d18cc6771fe242516
Aghas Seyranyan <aghasi.seyranyan@gmail.com>
2020-07-16 18:35:28 +0400

    Add project files.

---
+++ Homework-2(Calculator using classes)/Calculator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework_2_Calculator_using_classes_
+{
+    class Calculator
+    {
+        private readonly Double number1, number2;
+        private Double output;
+        private readonly Char optr;
+        public Calculator(double n1, double n2, char optr)
+        {
+            this.number1 = n1;
+            this.number2 = n2;
+            this.optr = optr;
+        }
+
+        public void Calculate()
+        {
+            switch (optr)
+            {
+                case '+':
+                    {
+
+                        output = number1 + number2;
+                    }
+
+                    break;
+                case '-':
+                    {
+
+                        output = number1 - number2;
+                    }
+
+                    break;
+                case '*':
+                    {
+
+                        output = number1 * number2;
+                    }
+
+                    break;
+                case '/':
+                    {
+
+                        output = number1 / number2;
+                    }
+                    break;
+
+            }
+            Console.WriteLine(output);
+
+        }
+    }
+}
---
+++ Homework-2(Calculator using classes)/ConsoleHost.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework_2_Calculator_using_classes_
+{
+   abstract class ConsoleHost
+    {
+        static public  void Run()
+        {
+            Console.WriteLine("Hello.This is Calculator");
+            Console.WriteLine("If you want to finish please enter  'end' ");
+            Console.WriteLine("You can use ',' for float numbers and /,*,-,+ as operators.");
+            while (true)
+
+            {
+                Console.WriteLine("Please enter a mathematical expression ");
+                var input = Console.ReadLine();
+                if (input == "end")
+                {
+                    break;
+                }
+                var Tester_obj = new Tester(input);
+                Tester_obj.Test();
+                var Calculator_obj = new Calculator(Tester_obj.Number1, Tester_obj.Number2, Tester_obj.Optr);
+                Calculator_obj.Calculate();
+            }
+        }
+    }
+}
